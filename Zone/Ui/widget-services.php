fdcddc864418547c5d76d8656e22930fc9e99a5d
Jérôme Augé <jerome.auge@anakeen.com>
2012-02-20 15:57:41 +0100

    Moved extui from platform to a dedicated module (refs #2443)

---
+++ Zone/Ui/widget-services.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<script src="<?php
+/*
+ * @author Anakeen
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
+ * @package FDL
+*/
+
+print $_GET['js']; ?>"></script>
+</head><body>
+</body></html>
